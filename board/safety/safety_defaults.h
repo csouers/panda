a9fec7aa55c720b057066949217bce9a20591c22
Chris Souers <csouers@gmail.com>
2021-05-20 14:08:14 -0400

    start faking acc/radar commands when OP is off

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -1,5 +1,88 @@
+void can_send(CAN_FIFOMailBox_TypeDef *to_push, uint8_t bus_number, bool skip_tx_hook);
+
+// refactor all of this down after it works
+uint8_t bus_fcan = 1U;
+uint8_t idx_1df = 0U;
+uint8_t idx_1ef = 0U;
+uint8_t idx_1fa = 0U;
+uint8_t idx_30c = 0U;
+uint8_t idx_39f = 0U;
+
+void send_1df(uint32_t RIR, uint32_t RDTR) {
+  CAN_FIFOMailBox_TypeDef to_send;
+  uint32_t addr_mask = 0x001FFFFF;
+  uint8_t msg_len = 8;
+  int msg_addr = 0x1df;
+  to_send.RIR = (msg_addr << 21) + (addr_mask & (RIR | 1));
+  to_send.RDTR = (RDTR & 0xFFFFFFF0) | msg_len;
+  to_send.RDLR = 0x0008d08a;
+  to_send.RDHR = 0x0;
+  can_send(&to_send, bus_fcan, true);
+}
+
+void send_1ef(uint32_t RIR, uint32_t RDTR) {
+  CAN_FIFOMailBox_TypeDef to_send;
+  uint32_t addr_mask = 0x001FFFFF;
+  uint8_t msg_len = 8;
+  int msg_addr = 0x1ef;
+  to_send.RIR = (msg_addr << 21) + (addr_mask & (RIR | 1));
+  to_send.RDTR = (RDTR & 0xFFFFFFF0) | msg_len;
+  to_send.RDLR = 0x7500FF03;
+  to_send.RDHR = 0x0;
+  can_send(&to_send, bus_fcan, true);
+}
+
+void send_1fa(uint32_t RIR, uint32_t RDTR) {
+  CAN_FIFOMailBox_TypeDef to_send;
+  uint32_t addr_mask = 0x001FFFFF;
+  uint8_t msg_len = 8;
+  int msg_addr = 0x1fa;
+  to_send.RIR = (msg_addr << 21) + (addr_mask & (RIR | 1));
+  to_send.RDTR = (RDTR & 0xFFFFFFF0) | msg_len;
+  to_send.RDLR = 0x0;
+  to_send.RDHR = 0x0;
+  can_send(&to_send, bus_fcan, true);
+}
+
+void send_30c(uint32_t RIR, uint32_t RDTR) {
+  CAN_FIFOMailBox_TypeDef to_send;
+  uint32_t addr_mask = 0x001FFFFF;
+  uint8_t msg_len = 8;
+  int msg_addr = 0x30c;
+  to_send.RIR = (msg_addr << 21) + (addr_mask & (RIR | 1));
+  to_send.RDTR = (RDTR & 0xFFFFFFF0) | msg_len;
+  to_send.RDLR = 0xFF000000;
+  to_send.RDHR = 0x00c0c019;
+  can_send(&to_send, bus_fcan, true);
+}
+
+void send_39f(uint32_t RIR, uint32_t RDTR) {
+  CAN_FIFOMailBox_TypeDef to_send;
+  uint32_t addr_mask = 0x001FFFFF;
+  uint8_t msg_len = 8;
+  int msg_addr = 0x39f;
+  to_send.RIR = (msg_addr << 21) + (addr_mask & (RIR | 1));
+  to_send.RDTR = (RDTR & 0xFFFFFFF0) | msg_len;
+  to_send.RDLR = 0x00003000;
+  to_send.RDHR = 0x0;
+  can_send(&to_send, bus_fcan, true);
+}
+
 int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
-  UNUSED(to_push);
+  int addr = GET_ADDR(to_push);
+  int bus = GET_BUS(to_push);
+
+  if (bus == 1){
+    if (addr == 0x1a4){
+      send_1df(to_push->RIR,to_push->RDTR);
+      send_1ef(to_push->RIR,to_push->RDTR);
+    }
+    if (addr == 0x309){
+      send_1fa(to_push->RIR,to_push->RDTR);
+      send_30c(to_push->RIR,to_push->RDTR);
+      send_39f(to_push->RIR,to_push->RDTR);
+    }
+  }
   return true;
 }
 
