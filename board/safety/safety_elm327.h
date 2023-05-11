bd6afed446bf749c532f3b0d2ed55872471f532f
Chris Souers <csouers@gmail.com>
2023-05-11 00:12:30 -0400

    that was missing

--- board/safety/safety_elm327.h
+++ board/safety/safety_elm327.h
@@ -40,7 +40,7 @@ static bool elm327_tx_hook(CANPacket_t *to_send) {
   if ((addr != 0x18DB33F1) && ((addr & 0x1FFF00FF) != 0x18DA00F1) &&
       ((addr & 0x1FFFFF00) != 0x600) && ((addr & 0x1FFFFF00) != 0x700)) {
     tx = false;
-  }
+    }
   }
   return tx;
 }
