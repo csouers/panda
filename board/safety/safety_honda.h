258634bb8e90ed255334fecadbbea24e46ac4f98
Chris Souers <csouers@gmail.com>
2024-10-21 02:03:43 -0400

    no forwarding

--- board/safety/safety_honda.h
+++ board/safety/safety_honda.h
@@ -447,21 +447,10 @@ static int honda_nidec_fwd_hook(int bus_num, int addr) {
 }
 
 static int honda_bosch_fwd_hook(int bus_num, int addr) {
-  int bus_fwd = -1;
+  UNUSED(bus_num);
+  UNUSED(addr);
 
-  if (bus_num == 0) {
-    bus_fwd = 2;
-  }
-  if (bus_num == 2)  {
-    bool is_lkas_msg = (addr == 0xE4) || (addr == 0xE5) || (addr == 0x33D) || (addr == 0x33DA) || (addr == 0x33DB);
-    bool is_acc_msg = ((addr == 0x1C8) || (addr == 0x30C)) && honda_bosch_radarless && honda_bosch_long;
-    bool block_msg = is_lkas_msg || is_acc_msg;
-    if (!block_msg) {
-      bus_fwd = 0;
-    }
-  }
-
-  return bus_fwd;
+  return -1;
 }
 
 const safety_hooks honda_nidec_hooks = {
