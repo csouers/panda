99c973d8453845bfd6e6eea4c22a0e4be3c93ae5
Chris Souers <csouers@gmail.com>
2024-10-21 01:58:41 -0400

    Revert "disable relay control"
    This reverts commit 8f7aaa39772e4a0ea11487f3902f282db74511f4.

--- board/drivers/harness.h
+++ board/drivers/harness.h
@@ -5,8 +5,7 @@ struct harness_t harness;
 // The ignition relay is only used for testing purposes
 void set_intercept_relay(bool intercept, bool ignition_relay) {
   if (current_board->harness_config->has_harness) {
-    // bool drive_relay = intercept;
-    bool drive_relay = false;
+    bool drive_relay = intercept;
     if (harness.status == HARNESS_STATUS_NC) {
       // no harness, no relay to drive
       drive_relay = false;
