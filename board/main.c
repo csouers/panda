152de75c564b756652ebc4999b1e6c538b4f3e3f
Chris Souers <csouers@gmail.com>
2024-10-21 23:25:41 -0400

    try again

--- board/main.c
+++ board/main.c
@@ -105,7 +105,7 @@ void set_safety_mode(uint16_t mode, uint16_t param) {
       can_silent = ALL_CAN_LIVE;
       break;
     default:
-      set_intercept_relay(true, false);
+      set_intercept_relay(false, false);
       heartbeat_counter = 0U;
       heartbeat_lost = false;
       if (current_board->has_obd) {
